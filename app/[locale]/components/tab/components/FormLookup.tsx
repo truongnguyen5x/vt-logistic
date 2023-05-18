49ca10b7dbaf01e78e252dec757c7a3a703de115
truong5x <truongnguyen5x@gmail.com>
2023-05-18 12:26:42 +0700

    fix

--- app/[locale]/components/tab/components/FormLookup.tsx
+++ app/[locale]/components/tab/components/FormLookup.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { FC, Fragment } from "react";
+import { FC, Fragment, useState } from "react";
 import { ILookupContent } from "..";
 import Image from "next/image";
 import styles from "@app/styles.module.scss";
@@ -9,6 +9,9 @@ import CustomSelect from "@components/custom-select";
 import SearchImg from "@assets/images/icons/search_light.svg";
 import { ICountry, IProvince } from "@type/location";
 import { SERVICE_TRANSPORT } from "@ultility/constant";
+import ServiceUnavailable from "./ServiceUnvailable";
+import ServiceAvailable from "./ServiceAvailable";
+import ChooseLocation from "./ChooseLocation";
 
 interface FormLookupProps {
   content: ILookupContent;
@@ -22,68 +25,36 @@ interface FormLookupProps {
   listProvince: IProvince[];
 }
 
+const STATUS = {
+  NONE: "NONE",
+  UNAVAILABLE: "UNAVAILABLE",
+  AVAILABLE: "AVAILABLE",
+};
+
 const FormLookup: FC<FormLookupProps> = ({
   content,
   index,
   listCountry,
   listProvince,
 }) => {
+  const [status, setStatus] = useState<"NONE" | "UNAVAILABLE" | "AVAILABLE">(
+    "NONE"
+  );
+
+  if (status == "UNAVAILABLE") {
+    return <ServiceUnavailable title={content.service_unavailable} />;
+  }
+  if (status == "AVAILABLE") {
+    return <ServiceAvailable content={content} />;
+  }
+
   return (
-    <div className={styles.lookUpBg}>
-      <Image
-        className="absolute right-2 top-2"
-        src={content.imgs[index]}
-        alt=""
-        width={685}
-        height={423}
-      />
-      <div className="container mx-auto flex justify-start p-12">
-        <div className="flex flex-col gap-2">
-          <div className="flex gap-7 mb-9">
-            <div className={styles.formIconWp}>
-              <Image alt="location" src={LocationImg} width={38} height={38} />
-              <div></div>
-              <Image alt="location" src={MapImg} width={38} height={38} />
-            </div>
-            <div className="flex-grow flex flex-col gap-4">
-              <p className={styles.formTitle}>{content.from}</p>
-              <CustomSelect>
-                {index == SERVICE_TRANSPORT.TRANSPORT
-                  ? listProvince.map((province, idx) => (
-                      <option key={idx} value={province.id}>
-                        {province.name}
-                      </option>
-                    ))
-                  : listCountry.map((country, idx) => (
-                      <option key={idx} value={country.id}>
-                        {country.name}
-                      </option>
-                    ))}
-              </CustomSelect>
-              <div className="mt-6" />
-              <p className={styles.formTitle}>{content.to}</p>
-              <CustomSelect>
-                {index == SERVICE_TRANSPORT.TRANSPORT
-                  ? listProvince.map((province, idx) => (
-                      <option key={idx} value={province.id}>
-                        {province.name}
-                      </option>
-                    ))
-                  : listCountry.map((country, idx) => (
-                      <option key={idx} value={country.id}>
-                        {country.name}
-                      </option>
-                    ))}
-              </CustomSelect>
-            </div>
-          </div>
-          <button className="btn-red">
-            {content.btn}
-            <Image src={SearchImg} alt="search" width={20} height={20} />
-          </button>
-        </div>
-      </div>
-    </div>
+    <ChooseLocation
+      listCountry={listCountry}
+      listProvince={listProvince}
+      content={content}
+      index={index}
+    />
   );
 };
 
