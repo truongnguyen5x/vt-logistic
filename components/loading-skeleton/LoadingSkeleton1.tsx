12245c2779271295e890e83d16f15c0a9a1e8738
truong5x <truongnguyen5x@gmail.com>
2023-05-19 09:30:13 +0700

    fix

---
+++ components/loading-skeleton/LoadingSkeleton1.tsx
@@ -0,0 +1,8 @@
+import { Fragment } from "react";
+import ContentLoader, { Facebook } from "react-content-loader";
+
+const LoadingSkeleton1 = () => {
+  return <Fragment></Fragment>;
+};
+
+export default LoadingSkeleton1;
