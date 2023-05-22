6b28199491f337d7fee7fff67103136613901217
truong5x <truongnguyen5x@gmail.com>
2023-05-22 14:18:39 +0700

    fix

--- app/[locale]/introduce/milestones/components/Content.tsx
+++ app/[locale]/introduce/milestones/components/Content.tsx
@@ -65,8 +65,7 @@ const Content: FC<Props> = ({ assets }) => {
                   ? "text-4xl text-th-red-500 font-semibold after:h-8 after:w-8 after:bg-th-red-500 after:-left-[125px] after:top-0"
                   : "font-medium text-[25px] text-th-gray-330 after:h-4 after:w-4 after:bg-th-gray-330 after:-left-[117px] after:top-2"
               } animation mb-[50px] last:mb-0
-            relative min-w-[85px] cursor-pointer text-end after:content-[''] after:absolute after:rounded-full
-            `}
+            relative min-w-[85px] cursor-pointer text-end after:content-[''] after:absolute after:rounded-full transition-all`}
               data-animation-delay={`${0.3 + 0.1 * index}s`}
               onClick={() => onSelect(item)}
             >
