49e4c5fc49198d7e310f69405c0ef15f5bd8ee82
truong5x <truongnguyen5x@gmail.com>
2023-05-30 14:41:54 +0700

    fix

--- components/BackToTop.tsx
+++ components/BackToTop.tsx
@@ -5,12 +5,11 @@ import Messenger from "@assets/images/icons/messenger.png";
 import BackTop from "@assets/images/icons/back_top.svg";
 import Link from "next/link";
 import { Fragment, useEffect, useState } from "react";
-import clsx from "clsx";
 import useWindowSize from "@hooks/use-window-size";
 
 const BackToTop = ({ txt }: { txt: string }) => {
   const [showBtn, setShowBtn] = useState<boolean>(false);
-  const { isDesktop } = useWindowSize();
+  const { isDesktop, isTablet } = useWindowSize();
 
   const toggleVisible = () => {
     if (
@@ -41,17 +40,19 @@ const BackToTop = ({ txt }: { txt: string }) => {
         href="http://m.me/LogisticsViettel"
         target="_blank"
         title="Contact us via Messenger"
-        className="z-20 fixed right-3  lg:right-[98px] bottom-5 lg:bottom-20 transition duration-700"
+        className="z-20 fixed right-3 xl:right-[98px] bottom-5 xl:bottom-20 transition duration-700"
         style={{
           transform: showBtn
             ? isDesktop
               ? "translateY(-90px)"
+              : isTablet
+              ? "translateY(-65px)"
               : "translateY(-40px)"
             : "",
         }}
       >
         <Image
-          className="w-11 md:w-16 lg:w-[70px] h-11 md:h-16 lg:h-[70px]"
+          className="w-11 md:w-16 xl:w-[70px] h-11 md:h-16 xl:h-[70px]"
           src={Messenger}
           alt=""
           height={70}
@@ -59,18 +60,18 @@ const BackToTop = ({ txt }: { txt: string }) => {
         />
       </Link>
       <div
-        className="z-10 fixed right-5 md:right-[26px] lg:right-20 bottom-5  lg:bottom-20 cursor-pointer flex flex-col gap-3 items-center overflow-hidden transition duration-5000"
+        className="z-10 fixed right-5 md:right-[25px] xl:right-[74px] bottom-5  xl:bottom-20 cursor-pointer flex flex-col gap-3 items-center overflow-hidden transition duration-5000"
         style={{ opacity: showBtn ? 1 : 0 }}
         onClick={handleBackToTop}
       >
         <Image
-          className="w-7 md:w-10 lg:w-10 h-7 md:h-10 lg:h-10"
+          className="w-7 md:w-10 xl:w-10 h-7 md:h-10 xl:h-10"
           src={BackTop}
           alt=""
           height={40}
           width={40}
         />
-        <p className="hidden lg:block font-medium text-lg">{txt}</p>
+        <p className="hidden xl:block font-medium text-xl">{txt}</p>
       </div>
     </Fragment>
   );
