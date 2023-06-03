24ec3ef5ecc1d15843b80fdd42e2b75bd676ed87
truongnguyen5x <truongnguyen5x@gmail.com>
2023-06-03 08:55:55 +0700

    fix map

---
+++ components/Map.tsx
@@ -0,0 +1,26 @@
+"use client";
+import React from "react";
+import { Map, Marker } from "pigeon-maps";
+import { maptiler } from 'pigeon-maps/providers'
+
+const MAPTILER_ACCESS_TOKEN = 'yviQnWhzph2Vh5cdZUXz'
+
+const maptilerProvider = maptiler(MAPTILER_ACCESS_TOKEN, 'dataviz-light')
+
+const MapContact = () => {
+  return (
+    <div className="relative px-4 md:px-6 xl:px-0 xl:absolute right-0 top-0 w-[343px] mx-auto md:w-[600px] xl:w-[520px] 2xl:w-[960px] h-[659px] max-xl:mb-16">
+      <Map
+        provider={maptilerProvider}
+        dprs={[1, 2]}
+        defaultCenter={[21.03047, 105.78326]}
+        defaultZoom={16}
+        boxClassname="animation"
+      >
+        <Marker width={50} anchor={[21.03047, 105.78326]} />
+      </Map>
+    </div>
+  );
+};
+
+export default React.memo(MapContact);
